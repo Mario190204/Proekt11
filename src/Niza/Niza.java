3317b0d745fc061781ea9ad9b123bdaee27d8fb2
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 11:33:27 +0100

    Proekt11

---
+++ src/Niza/Niza.java
@@ -0,0 +1,23 @@
+package Niza;
+import java.util.Scanner;
+public class Niza {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		int i,n;
+		System.out.println("Do brojot:");
+		n=input.nextInt();
+		i=1;
+		while(i<=n);
+		{
+			if(i%4==0 && i%7==0);
+			{
+				System.out.println(i);
+			}
+		}
+			
+
+	}
+
+}
